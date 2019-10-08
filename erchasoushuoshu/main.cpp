555ac12302472871fbd94db14d1072eeccf7bb4d
hw20181102929 <51589984+hw20181102929@users.noreply.github.com>
2019-10-08 17:27:22 +0800

    Update main.cpp

--- erchasoushuoshu/main.cpp
+++ erchasoushuoshu/main.cpp
@@ -1,15 +1,49 @@
-//
-//  main.cpp
-//  erchasoushuoshu
-//
-//  Created by s20181102929 on 2019/10/8.
-//  Copyright © 2019 s20181102929. All rights reserved.
-//
+#include <stdio.h>
+#include <stdlib.h>
 
-#include <iostream>
+typedef enum { false, true } bool;
+typedef int ElementType;
+typedef struct TNode *Position;
+typedef Position BinTree;
+struct TNode{
+    ElementType Data;
+    BinTree Left;
+    BinTree Right;
+};
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+BinTree BuildTree(); /* 由裁判实现，细节不表 */
+bool IsBST ( BinTree T );
+
+int main()
+{
+    BinTree T;
+    
+    T = BuildTree();
+    if ( IsBST(T) ) printf("Yes\n");
+    else printf("No\n");
+    
     return 0;
+}
+bool IsBST ( BinTree T )
+{
+    if(T!=NULL&&T->Right==NULL&&T->Left==NULL)
+    {
+        return true;
+    }
+    if(T!=NULL)
+    {
+    if(T->Data>IsBST(T->Right))
+    {
+        return false;
+    }
+   else if(T->Data<IsBST(T->Left))
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+    }
+        return true;
 }
